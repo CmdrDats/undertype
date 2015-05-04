8edc7e1dfc4a6687365e21a9368d4fd21892ab36
Deon Moolman <deon@yuppiechef.com>
2015-05-05 01:19:00 +0200

    Wrong compilation committed!

--- js/compiled/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [om-tut.core] [figwheel.client]))
-(figwheel.client/start {:websocket-url "ws://localhost:3449/figwheel-ws", :on-jsload om-tut.core/on-js-reload, :build-id "dev"})
-
--- js/compiled/out/figwheel/connect.js
+++
@@ -1,8 +0,0 @@
-// Compiled by ClojureScript 0.0-3211 {}
-goog.provide('figwheel.connect');
-goog.require('cljs.core');
-goog.require('figwheel.client');
-goog.require('om_tut.core');
-figwheel.client.start.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"websocket-url","websocket-url",-490444938),"ws://localhost:3449/figwheel-ws",new cljs.core.Keyword(null,"on-jsload","on-jsload",-395756602),om_tut.core.on_js_reload,new cljs.core.Keyword(null,"build-id","build-id",1642831089),"dev"], null));
-
-//# sourceMappingURL=connect.js.map?rel=1430779896411
